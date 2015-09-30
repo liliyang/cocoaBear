3f9af0e1924d9d6faaecca07aebc42eb36b87ae7
snail <yy0222@stanford.edu>
2015-09-29 21:14:48 -0700

    Model 1 with AER:0.622

--- pa1/java/src/cs224n/wordaligner/IBMModel1Aligner.java
+++ pa1/java/src/cs224n/wordaligner/IBMModel1Aligner.java
@@ -19,22 +19,82 @@ public class IBMModel1Aligner implements WordAligner {
   
   // TODO: Use arrays or Counters for collecting sufficient statistics
   // from the training data.
-  private CounterMap<String,String> sourceTargetCounts;
+//  private CounterMap<String,String> sourceTargetCounts;
+  private CounterMap<String,String> countAlignment; //target, src
+  private CounterMap<String,String> transProb; //tgt, src
 
   public Alignment align(SentencePair sentencePair) {
     // Placeholder code below. 
     // TODO Implement an inference algorithm for Eq.1 in the assignment
     // handout to predict alignments based on the counts you collected with train().
     Alignment alignment = new Alignment();
-
+    List<String> sourceWords = sentencePair.getSourceWords();
+    List<String> targetWords = sentencePair.getTargetWords();
+    double maxScore, score;
+    int sourceId, targetId = 0;
     // YOUR CODE HERE
+    for (int i = 0; i < sourceWords.size(); i++) {
+    	sourceId = i;
+    	maxScore = 0.0;
+  		for (int j= 0; j < targetWords.size(); j++) {
+  			score = transProb.getCount(targetWords.get(j), sourceWords.get(i));
+  			if (score > maxScore) {
+  				maxScore = score;
+  				targetId = j;
+  			}
+  		}
+  		alignment.addPredictedAlignment(targetId, sourceId);
+  	}
    
     return alignment;
   }
 
   public void train(List<SentencePair> trainingPairs) {
-    
-    // YOUR CODE HERE
+	  double sum;
+	  
+	// start with uniform t
+	  countAlignment = new CounterMap<String, String>();
+	  transProb = new CounterMap<String,String>();
+	  List<String> sourceWords, targetWords;
+	  for (SentencePair p : trainingPairs) {
+		  sourceWords = p.getSourceWords();
+		  targetWords = p.getTargetWords();
+		  for (String sWord : sourceWords) {
+			  for (String tWord : targetWords) {
+				  transProb.setCount(tWord, sWord, 0.1);			  
+			  }
+		  }
+	  }
 
+	// iterate 
+	  for (int i = 0; i < 100; i++){
+		  for (SentencePair p : trainingPairs) {
+			  sourceWords = p.getSourceWords();
+			  targetWords = p.getTargetWords();
+			  for (String sWord : sourceWords) {
+				  
+				  // calc the sum once
+				  sum = 0.0;
+				  for (String tWord : targetWords) {
+					  sum += transProb.getCount(tWord, sWord);
+				  }				  
+				
+				  for (String tWord : targetWords) {		  
+					  countAlignment.incrementCount(tWord, sWord, transProb.getCount(tWord, sWord)/sum);
+				  }
+			  }
+		  }
+		  
+		  //Renormalize counts
+		  double sCountTotal;
+		  Counter<String> sCounter;
+		  for (String tWord: transProb.keySet()){
+			  sCounter = transProb.getCounter(tWord);
+			  sCountTotal = countAlignment.getCounter(tWord).totalCount();
+			  for (String sWord: sCounter.keySet()){
+				  transProb.setCount(tWord, sWord, countAlignment.getCount(tWord, sWord) / sCountTotal);
+			  }
+		  }
+	  }
   }
 }
