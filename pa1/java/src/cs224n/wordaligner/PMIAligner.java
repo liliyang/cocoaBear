ede604490f940bcfc6f65a26e7950d6bc7936749
Lili Yang <yanglili369@gmail.com>
2015-09-29 20:01:31 -0700

    added PMIAligner model

--- pa1/java/src/cs224n/wordaligner/PMIAligner.java
+++ pa1/java/src/cs224n/wordaligner/PMIAligner.java
@@ -20,21 +20,55 @@ public class PMIAligner implements WordAligner {
   // TODO: Use arrays or Counters for collecting sufficient statistics
   // from the training data.
   private CounterMap<String,String> sourceTargetCounts;
+  private Counter<String> sourceCounts;
+  private Counter<String> targetCounts;
 
   public Alignment align(SentencePair sentencePair) {
     // Placeholder code below. 
     // TODO Implement an inference algorithm for Eq.1 in the assignment
     // handout to predict alignments based on the counts you collected with train().
     Alignment alignment = new Alignment();
-
-    // YOUR CODE HERE
-   
+    List<String> sourceWords = sentencePair.getSourceWords();
+    List<String> targetWords = sentencePair.getTargetWords();
+    double maxScore, score;
+    int sourceId, targetId = 0;
+    
+    for (int i = 0; i < sourceWords.size(); i++) {
+    	sourceId = i;
+    	maxScore = 0.0;
+  		for (int j= 0; j < targetWords.size(); j++) {
+  			score = scoreAlignment(sourceWords.get(i), targetWords.get(j));
+  			if (score > maxScore) {
+  				maxScore = score;
+  				targetId = j;
+  			}
+  		}
+  		alignment.addPredictedAlignment(targetId, sourceId);
+  	}
     return alignment;
   }
 
   public void train(List<SentencePair> trainingPairs) {
-    
-    // YOUR CODE HERE
-
+    List<String> sourceWords;
+    List<String> targetWords;
+  	
+    for (SentencePair p : trainingPairs) {
+    	sourceWords = p.getSourceWords();
+    	targetWords = p.getTargetWords();
+    	for (String sWord : sourceWords) {
+    		sourceCounts.incrementCount(sWord, 1.0);
+    		for (String tWord : targetWords) {
+    			targetCounts.incrementCount(tWord, 1.0);
+    			sourceTargetCounts.incrementCount(sWord, tWord, 1.0);
+    		}
+    	}
+    }
+  }
+     
+  public double scoreAlignment(String sWord, String tWord) {
+  	double sCount = sourceCounts.getCount(sWord) + 1;
+  	double tCount = targetCounts.getCount(tWord) + 1;
+  	double count = sourceTargetCounts.getCount(sWord, tWord) + 1;
+  	return count/(sCount*tCount);
   }
 }
