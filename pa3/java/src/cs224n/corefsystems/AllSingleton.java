d0a3888407e9897f06e681164f6227ebae2f237e
Lili Yang <yanglili369@gmail.com>
2015-10-31 22:57:24 -0700

    implemented naive baseline

--- pa3/java/src/cs224n/corefsystems/AllSingleton.java
+++ pa3/java/src/cs224n/corefsystems/AllSingleton.java
@@ -1,25 +1,32 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class AllSingleton implements CoreferenceSystem {
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
+		// Nothing to do here 
 
 	}
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
+		// iterate through all mentions
+		for(Mention m : doc.getMentions()){
+			// We mark each mention as its own entity
+			ClusteredMention newCluster = m.markSingleton();
+      mentions.add(newCluster);
+		}
+		return mentions;
 	}
 
 }
--- pa3/java/src/cs224n/corefsystems/OneCluster.java
+++ pa3/java/src/cs224n/corefsystems/OneCluster.java
@@ -1,25 +1,38 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class OneCluster implements CoreferenceSystem {
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
+		// Nothing to do here
 
 	}
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
+		// iterate through all mentions
+		ClusteredMention cluster = null;
+		for(Mention m : doc.getMentions()){
+			// if there's no cluster, create a new cluster
+			if (cluster == null) {
+				cluster = m.markSingleton();
+				mentions.add(cluster);
+			} else {
+				// otherwise, just add mention to cluster
+				mentions.add(m.markCoreferent(cluster.entity));;
+			}
+		}
+		return mentions;
 	}
 
 }
